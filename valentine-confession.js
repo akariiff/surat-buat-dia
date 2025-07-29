db21d06408327c33f672d1e6be2b446be15846d6
akariiff <akarisecaccound@gmail.com>
2025-07-29 14:15:57 +0700

    Update valentine-confession.js

--- valentine-confession.js
+++ valentine-confession.js
@@ -1,30 +1,23 @@
-$("#messageState").on("change", (x) => {
-	$(".message").removeClass("openNor").removeClass("closeNor");
-	if ($("#messageState").is(":checked")) {
-		$(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
-		$(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
-		$(".container").stop().animate({"backgroundColor": "#f48fb1"}, 2000);
-		console.log("Abrindo");
-	} else {
-		$(".message").removeClass("no-anim").addClass("closeNor");
-		$(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
-		$(".container").stop().animate({"backgroundColor": "#fce4ec"}, 2000);
-		console.log("fechando");
-	}
-});
+$(document).ready(function () {
+  // semua kode lama kamu masuk sini
 
-$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	console.log("Animation End");
-	if ($(".message").hasClass("closeNor"))
-		$(".message").addClass("closed");
-	$(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
-});
+  var checkbox = $('#messageState');
+  var message = $('.message');
+  var heart = $('.heart');
 
-$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	console.log("Animation End");
-	if (!$(".heart").hasClass("closeHer"))
-		$(".heart").addClass("openedHer").addClass("beating");
-	else
-		$(".heart").addClass("no-anim").removeClass("beating");
-	$(".heart").removeClass("openHer").removeClass("closeHer");
+  checkbox.change(function () {
+    if (this.checked) {
+      message.removeClass('closeNor');
+      message.addClass('openNor');
+
+      heart.removeClass('closeHer');
+      heart.addClass('openHer');
+    } else {
+      message.removeClass('openNor');
+      message.addClass('closeNor');
+
+      heart.removeClass('openHer');
+      heart.addClass('closeHer');
+    }
+  });
 });
