a3f67cc65c4051f6e6ae74374a018986fcac4f86
akariiff <akarisecaccound@gmail.com>
2025-07-29 14:19:42 +0700

    Update valentine-confession.js

--- valentine-confession.js
+++ valentine-confession.js
@@ -1,23 +1,15 @@
 $(document).ready(function () {
-  // semua kode lama kamu masuk sini
-
   var checkbox = $('#messageState');
   var message = $('.message');
   var heart = $('.heart');
 
   checkbox.change(function () {
     if (this.checked) {
-      message.removeClass('closeNor');
-      message.addClass('openNor');
-
-      heart.removeClass('closeHer');
-      heart.addClass('openHer');
+      message.removeClass('closeNor').addClass('openNor');
+      heart.removeClass('closeHer').addClass('openHer');
     } else {
-      message.removeClass('openNor');
-      message.addClass('closeNor');
-
-      heart.removeClass('openHer');
-      heart.addClass('closeHer');
+      message.removeClass('openNor').addClass('closeNor');
+      heart.removeClass('openHer').addClass('closeHer');
     }
   });
 });
