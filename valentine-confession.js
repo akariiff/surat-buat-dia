6debd047151201855dd47265994837826abc7a3a
akariiff <akarisecaccound@gmail.com>
2025-07-29 15:39:07 +0700

    Update valentine-confession.js

--- valentine-confession.js
+++ valentine-confession.js
@@ -1,14 +1,51 @@
-$(document).ready(function () {
-  var checkbox = $('#messageState');
-  var message = $('.message');
-  var heart = $('.heart');
+const slideEl = document.getElementById('slides');
+const bgm = document.getElementById('bgm');
+let currentSlide = 0;
 
-  checkbox.change(function () {
+function updateSlide() {
+  slideEl.style.transform = `translateX(-${currentSlide * 100}vw)`;
+
+  // Musik mati di slide video (slide ke-2)
+  if (currentSlide === 1) {
+    bgm.pause();
+  } else {
+    bgm.play().catch(() => {});
+  }
+
+  // Load bunga di slide 3
+  if (currentSlide === 2) {
+    $('#flowersContent').load('https://akariiff.github.io/flowers-for-her/', function (resp, status) {
+      if (status !== 'success') {
+        $('#flowersContent').html('<p>Gagal memuat bunga. Coba buka ulang halaman.</p>');
+      }
+    });
+  }
+}
+
+function nextSlide() {
+  if (currentSlide < 2) {
+    currentSlide++;
+    updateSlide();
+  }
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateSlide();
+  }
+}
+
+$(document).ready(function () {
+  $('#messageState').change(function () {
+    const msg = $('.message'), heart = $('.heart');
+
     if (this.checked) {
-      message.removeClass('closeNor').addClass('openNor');
+      bgm.play().catch(() => {}); // Play musik setelah interaksi
+      msg.removeClass('closeNor').addClass('openNor');
       heart.removeClass('closeHer').addClass('openHer');
     } else {
-      message.removeClass('openNor').addClass('closeNor');
+      msg.removeClass('openNor').addClass('closeNor');
       heart.removeClass('openHer').addClass('closeHer');
     }
   });
